7e993260c96ab39c8fa9f839bbfb6178104e9e56
cevio <evio@vip.qq.com>
2014-11-30 12:53:10 +0800

    update

--- blog/public/server/async/category.js
+++ blog/public/server/async/category.js
@@ -1,8 +1,10 @@
 var category = new Class(function(querys, getforms){
 	querys.m = querys.m || '';
 	
+	var categoryPromise = require(':public/library/category');
+	
 	if ( querys.m.length > 0 && this[querys.m] ){
-		return this[querys.m](querys, getforms);
+		return this[querys.m](querys, getforms, categoryPromise);
 	}else{
 		return { success: false, message: '找不到模块' };
 	}

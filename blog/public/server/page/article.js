e3e42bf32301bdafcb5c40278a1bd82b1513edfa
cevio <evio@vip.qq.com>
2014-12-12 18:34:49 +0800

    update

--- blog/public/server/page/article.js
+++ blog/public/server/page/article.js
@@ -2,6 +2,8 @@ var layout = new Class(function( querys, forms ){
     var that = this;
 	this.load(querys, forms);
 	this.getArticle();
+	this.getPrevArticle();
+	this.getNextArticle();
 	
 	fs(contrast(':private/themes/' + this.data.global.blog_theme + '/views/article.asp')).exist().then(function(){
 		that.render('article.asp');
@@ -14,7 +16,7 @@ var layout = new Class(function( querys, forms ){
 });
 
 layout.add('getArticle', function(){
-    var id = this.req.id;
+    var id = this.req.query.id;
     if ( !id || id.length === 0 ){
         id = 0;
     };
@@ -22,10 +24,41 @@ layout.add('getArticle', function(){
     if ( id < 1 ){
         this.error(10001);
     }else{
+		this.req.query.id = id;
         var articles = require(':public/library/article');
         var article = new articles();
         this.data.article = article.getArticleByID(id);
     }
+});
+
+layout.add('getPrevArticle', function(){
+	var rec = new dbo(blog.tb + 'articles', blog.conn);
+	var data = null;
+	
+	rec.top(1).select('id', 'art_title', 'art_cover').and('id', this.req.query.id, '<').desc('id').open().exec(function( object ){
+		data = {};
+		data['id'] = object('id').value;
+		data['art_title'] = object('art_title').value;
+		data['art_cover'] = object('art_cover').value;
+		data['src'] = iPress.setURL('page', 'article', { id: object('id').value });
+	}).close();
+	
+	this.data.PrevArticle = data;
+});
+
+layout.add('getNextArticle', function(){
+	var rec = new dbo(blog.tb + 'articles', blog.conn);
+	var data = null;
+	
+	rec.top(1).select('id', 'art_title', 'art_cover').and('id', this.req.query.id, '>').asc('id').open().exec(function( object ){
+		data = {};
+		data['id'] = object('id').value;
+		data['art_title'] = object('art_title').value;
+		data['art_cover'] = object('art_cover').value;
+		data['src'] = iPress.setURL('page', 'article', { id: object('id').value });
+	}).close();
+	
+	this.data.NextArticle = data;
 });
 
 layout.extend(require(':public/library/layout'));
